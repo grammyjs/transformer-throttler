7bebd1695f2a32264961ff7a308eb021e0a2ac89
Curtis Tan <merwinwee@gmail.com>
2021-05-11 21:05:07 +0800

    Create throttler transformer for grammy

---
+++ deps.ts
@@ -0,0 +1,2 @@
+export type { Transformer } from 'https://dsr.edjopato.de/grammy/^1.0.0/mod.ts';
+export { default as Bottleneck } from 'https://cdn.skypack.dev/bottleneck@^2.0.0?dts';
---
+++ mod.ts
@@ -0,0 +1,54 @@
+import { Bottleneck } from './deps.ts';
+import type { Transformer } from './deps.ts';
+
+type APIThrottlerOptions = {
+  global?: Bottleneck.ConstructorOptions,
+  group?: Bottleneck.ConstructorOptions,
+  out?: Bottleneck.ConstructorOptions,
+};
+
+const apiThrottler = (
+  opts: APIThrottlerOptions = {},
+): Transformer => {
+  const globalConfig: Bottleneck.ConstructorOptions = opts.global ?? {
+    reservoir: 30,
+    reservoirRefreshAmount: 30,
+    reservoirRefreshInterval: 1000,
+  };
+  const groupConfig: Bottleneck.ConstructorOptions = opts.group ?? {
+    maxConcurrent: 1,
+    minTime: 1000,
+    reservoir: 20,
+    reservoirRefreshAmount: 20,
+    reservoirRefreshInterval: 60000,
+  };
+  const outConfig: Bottleneck.ConstructorOptions = opts.out ?? {
+    maxConcurrent: 1,
+    minTime: 1000,
+  };
+
+  const globalThrottler = new Bottleneck(globalConfig);
+  const groupThrottler = new Bottleneck.Group(groupConfig);
+  const outThrottler = new Bottleneck.Group(outConfig);
+  groupThrottler.on('created', (throttler: Bottleneck) => throttler.chain(globalThrottler));
+  outThrottler.on('created', (throttler: Bottleneck) => throttler.chain(globalThrottler));
+
+  const transformer: Transformer = async (prev, method, payload) => {
+    if (!payload || !('chat_id' in payload)) {
+      return prev(method, payload);
+    }
+
+    // @ts-ignore
+    const chatId = Number(payload.chat_id);
+    const isGroup = chatId < 0;
+    const throttler = isGroup ? groupThrottler.key(`${chatId}`) : outThrottler;
+    return throttler.schedule(() => prev(method, payload));
+  };
+  return transformer;
+};
+
+const BottleneckStrategy = Bottleneck.strategy;
+export { BottleneckStrategy };
+
+export type { APIThrottlerOptions };
+export { apiThrottler };
